28a206c0e358766270406773038fc2f34366898d
MiltonCarreno <miltonyaredcr@gmail.com>
2023-10-22 16:37:08 -0700

    Error handling: arguments parsing and file errors

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        return Ok(Config{ query, file_path });
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("\nWith text: \n{contents}");
+    return Ok(());
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args)
+    .unwrap_or_else(|err| {
+        println!("\nProblem parsing arguments: {err}\n");
+        process::exit(1);
+    });
+    println!("Searching for \"{}\"", config.query);
+    println!("In file: {}", config.file_path);
 
-    println!("Searching for \"{}\"", query);
-    println!("In file: {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file");
-
-    println!("\nWith text: \n{contents}");
+    if let Err(e) = run(config) {
+        println!("\nApplication error: {e}\n");
+        process::exit(1);
+    }
 }
