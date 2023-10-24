0795025ba1b3b634b23fabaeab959b596a92c6ce
MiltonCarreno <miltonyaredcr@gmail.com>
2023-10-24 11:44:31 -0700

    Replaced loops with iterators

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args)
+    let config = Config::build(env::args())
     .unwrap_or_else(|err| {
         eprintln!("\nProblem parsing arguments: {err}\n");
         process::exit(1);
